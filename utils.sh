9c3cc72776b98b87b85d17211d6da69a1925d313
Ramesh Babu <rameshbabu.thoomu@gmail.com>
2018-09-30 20:10:41 -0400

    Create utils.sh

---
+++ utils.sh
@@ -0,0 +1,159 @@
+#!/bin/bash
+
+# exit on the first error
+set -o pipefail
+
+# set targets here
+FABRIC_1_0_TARGETS=(docker release-clean release)
+FABRIC_TARGETS=(docker release-clean release docker-thirdparty)
+FABRIC_CA_TARGETS=(docker)
+FABRIC_CA_1_0_TARGETS=(docker-fabric-ca)
+FABRIC_IMAGES=(peer orderer ccenv tools)
+FABRIC_CA_IMAGES=(ca)
+BASE_DIR=${WORKSPACE}/gopath/src/github.com/hyperledger
+NEXUS_BASE_URL=nexus3.hyperledger.org:10001
+ORG_NAME=hyperledger/fabric
+JAVA_IMAGE=javaenv
+WD="${WORKSPACE}/gopath/src/github.com/hyperledger/${PROJECT}"
+
+# get arch
+function getArch() {
+    ARCH=$(go env GOARCH)
+    if [ "$ARCH" = "amd64" ]; then
+	    ARCH=amd64
+    else
+        ARCH=$(uname -m)
+    fi
+}
+
+# Here $1 is the project name $2 is branch name
+function cloneRepo(){
+        projectName=$1
+        branchName=$2
+        echo "########## ${projectName}"
+        # Clone repository
+        git clone --single-branch -b ${branchName} --depth=1 git://cloud.hyperledger.org/mirror/$FABRIC_REPO_NAME $WD
+        echo "Clone and checkout to the given branch"
+        git checkout ${branchName} > /dev/null 2>&1
+        if [ $? -ne 0 ]; then
+                echo "------> Branch ${branchName} not found - Checkout to master"
+                git checkout master
+        fi
+        if [ -d ${WD} ]; then # if directory exists
+        # Be at project root directory
+            cd ${WD}
+            COMMIT=$(git log -1 --pretty=format:"%h")
+            echo "------> "Commit SHA is ${COMMIT}"
+        else
+            echo "------> Directory not found. Clone again"
+            exit 1
+        fi
+} # close cloneRepo
+
+## Here $1 is the project name, $2 is branch name
+function buildImages(){
+        projectName=$1
+        branchName=$2
+        echo "########## ${projectName}"
+        if [ ${projectName} = "fabric" ]; then
+                echo "------> Build artifacts for ${projectName}"
+                if [ "${branchName}" = "release-1.0" ] ; then # only on release-1.0 branch
+                       for ${TARGET} in ${FABRIC_1_0_TARGETS[*]}; do
+                            # Build artifacts
+                            make ${TARGET} > /dev/null 2>&1
+                            if [ $? -ne 0 ]; then
+                                echo "ERROR: ------> make ${TARGET} failed"
+                                exit 1
+                            fi
+                       done
+                else # otherthan release-1.0 branch
+                    for ${TARGET} in ${FABRIC_TARGETS}
+                        # Build artifacts
+                        make ${TARGET} > /dev/null 2>&1
+                        if [ $? -ne 0 ]; then
+                            echo "ERROR: ------> make ${TARGET} failed"
+                            exit 1
+                        fi
+                    done
+                fi
+        elif [ ${projectName} = "fabric-ca" ]; then
+                echo "------> Build artifacts for ${projectName}"
+                if [ ${branchName} = "release-1.0" ] ; then # only on release-1.0 branch
+                       for ${TARGET} in ${FABRIC_CA_1_0_TARGETS[*]}; do
+                            # Build artifacts
+                            make ${TARGET} > /dev/null 2>&1
+                            if [ $? -ne 0 ]; then
+                                echo "ERROR: ------> make ${TARGET} failed"
+                                exit 1
+                            fi
+                       done
+                else # otherthan release-1.0 branch
+                    for ${TARGET} in ${FABRIC_CA_TARGETS}
+                        # Build artifacts
+                        make ${TARGET} > /dev/null 2>&1
+                        if [ $? -ne 0 ]; then
+                            echo "ERROR: ------> make ${TARGET} failed"
+                            exit 1
+                        fi
+                    done 
+                fi
+        fi
+} # Close for buildImages
+
+function buildAllImages() {
+        BRANCH_NAME=$1
+        getArch
+        export_Go fabric
+        cloneRepo fabric branch_name
+        buildImages fabric branch_name
+        export_Go fabric-ca
+        cloneRepo fabric-ca branch_name
+        buildImages fabric-ca branch_name
+
+} # Close buildAllImages
+
+function export_Go{
+    echo "-------> Export GOPATH"
+    cd ${BASE_DIR}/${projectName}
+    GO_VER=$(cat ci.properties | grep GO_VER | cut -d "=" -f 2)
+    echo "------> GO_VER" ${GO_VER}
+    OS_VER=$(dpkg --print-architecture)
+    echo "------> OS_VER" ${OS_VER}
+    export GOROOT=/opt/go/go${GO_VER}.linux.${OS_VER}
+    export PATH=${GOROOT}/bin:${PATH}
+}
+
+function pullJavaImages(){
+        getArch
+        echo "------> Pull Docker Images"
+        if [ ${ARCH} -eq "amd64" ]; then # JAVAENV is not available on other platforms
+            if [ "${branchName}" = "master" ]; then
+                export STABLE_VERSION=1.4.0-stable
+                export JAVA_ENV_TAG=1.4.0
+            else
+                export STABLE_VERSION=1.3.0-stable
+                export JAVA_ENV_TAG=1.3.0
+            fi
+            docker pull $NEXUS_BASE_URL/$ORG_NAME-$IMAGE:$ARCH-$STABLE_VERSION
+            docker tag $NEXUS_BASE_URL/$ORG_NAME-$IMAGE:$ARCH-$STABLE_VERSION $ORG_NAME-$IMAGE
+            docker tag $NEXUS_BASE_URL/$ORG_NAME-$IMAGE:$ARCH-$STABLE_VERSION $ORG_NAME-$IMAGE:${ARCH}-$JAVA_ENV_TAG
+        fi
+}
+
+# pull fabric-ca docker images
+function pull_Images() {
+        getArch
+        # export image list
+        if [ "${projectName}" = "fabric" ]; then
+            export IMAGES_LIST=${FABRIC_IMAGES}
+        else
+            export IMAGES_LIST=${FABRIC_CA_IMAGES}
+        fi
+        # pull images
+        for IMAGES in ${IMAGES_LIST[*]}; do
+            docker pull $NEXUS_BASE_URL/$ORG_NAME-$IMAGES:$ARCH-$STABLE_VERSION
+            docker tag $NEXUS_BASE_URL/$ORG_NAME-$IMAGES:$ARCH-$STABLE_VERSION $ORG_NAME-$IMAGES:$ARCH-$RELEASE_VERSION
+            docker tag $NEXUS_BASE_URL/$ORG_NAME-$IMAGES:$ARCH-$STABLE_VERSION $ORG_NAME-$IMAGES
+            docker rmi -f $NEXUS_BASE_URL/$ORG_NAME-$IMAGES:$ARCH-$STABLE_VERSION
+        done
+}
